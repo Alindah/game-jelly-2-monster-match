c2012d7247000b1d0497a8c670f053f4074fedb4
Alinda <Alindah@users.noreply.github.com>
2022-09-12 14:39:56 -0500

    Comments on traits.cs

--- Assets/Scripts/Traits.cs
+++ Assets/Scripts/Traits.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class Traits : MonoBehaviour
 {
+    // Need to be List<string> instead of array to add to TMP_Dropdown type as options
     public static List<string> traits = new List<string>
     {
+        // Traits
         "Sleepy",
         "Sad",
         "Angry",
@@ -13,6 +15,7 @@ public class Traits : MonoBehaviour
         "Evil",
         "Serious",
 
+        // Opposing traits - order must correspond with above traits
         "Energetic",
         "Happy",
         "Calm",
