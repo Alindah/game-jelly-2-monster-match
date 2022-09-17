de5ea7e8d01e07da7ccffceb13920704695667d9
Alinda <Alindah@users.noreply.github.com>
2022-09-17 14:55:12 -0500

    Spotlight

--- Assets/Scripts/ConclusionsManager.cs
+++ Assets/Scripts/ConclusionsManager.cs
@@ -7,13 +7,34 @@ public class ConclusionsManager : MonoBehaviour
 {
     public TMP_Text matchesHeader;
     public TMP_Text rejectionsHeader;
+    public TMP_Text compatibilityHeader;
+
+    [Header("Spotlight")]
+    public TMP_Text infoHeader;
+    public TMP_Text traitsList;
+
+    private List<Monster> allMonsters;
 
     private void Start()
     {
+        // Create a list of all monsters, both matched and rejected
+        allMonsters = matches;
+        allMonsters.InsertRange(matches.Count, rejections);
+
+        // Display number of matches and rejections
         matchesHeader.text = string.Format(matchesHeader.text, matches.Count);
         rejectionsHeader.text = string.Format(rejectionsHeader.text, rejections.Count);
+
+        // Display monster cards
         DisplayMonsters(matches);
         DisplayMonsters(rejections);
+
+        // If player rejected all monsters, display player in  spotlight
+        //if (matches.Count == 0 && rejections.Count == 0)
+        //    SpotlightMonster(player)
+
+        // Spotlight first monster
+        SpotlightMonster(allMonsters[0]);
     }
 
     private void DisplayMonsters(List<Monster> monsters)
@@ -23,5 +44,9 @@ public class ConclusionsManager : MonoBehaviour
         {
             Debug.Log(string.Format("{0}, {1}", m.name, m.age));
         }
+    }
+
+    private void SpotlightMonster(Monster monster)
+    {
     }
 }
--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -5,12 +5,16 @@ public class SaveManager : MonoBehaviour
 {
     public static SaveManager Instance;
 
+    public static Monster player;
     public static string playerName = "Quasimodo";
     public static string playerAge = "1000";
     public static List<int> playerTraits;
     public static List<int> opposingTraits;
     public static List<Monster> matches;
     public static List<Monster> rejections;
+
+    private const string DEFAULT_PLAYER_NAME = "Quasimodo";
+    private const string DEFAULT_PLAYER_AGE = "1000";
 
     private void Awake()
     {
@@ -25,10 +29,17 @@ public class SaveManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
+        
+
         // Create lists
         playerTraits = new List<int>();
         opposingTraits = new List<int>();
         matches = new List<Monster>();
         rejections = new List<Monster>();
+    }
+
+    private void InitializePlayer()
+    {
+        player = new Monster();
     }
 }
