9cec75145ae4e971d9599a02f4066a4315ea3d18
Alinda <Alindah@users.noreply.github.com>
2022-09-23 18:17:01 -0400

    Created random age generator

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -18,7 +18,7 @@ public class Monster
     {
         // Info
         name = NamesList.GenerateRandomName();
-        age = Random.Range(0, 1001).ToString();
+        age = GenerateRandomAge(5, 65, 1.0f, 10.0f, 25);
         traits = Traits.SelectRandomTraits();
         compatibility = 0;
 
@@ -26,5 +26,17 @@ public class Monster
         baseColor = MonsterParts.RandomizeBaseColor();
         bodyParts = new GameObject[MonsterParts.numOfPartsCategories];
         bodyPartsInt = MonsterParts.RandomizeParts();
+    }
+
+    // Random age generator
+    private string GenerateRandomAge(int minIn, int maxEx, float multiplierMin = 1, float multiplierMax = 10, int multiplierChance = 10)
+    {
+        int age = Random.Range(minIn, maxEx);
+
+        // Chance to multiply for an ancient monster
+        if (Random.Range(0, 100) <= Random.Range(0, multiplierChance))
+            age *= (int)Random.Range(multiplierMin, multiplierMax);
+
+        return age.ToString();
     }
 }
